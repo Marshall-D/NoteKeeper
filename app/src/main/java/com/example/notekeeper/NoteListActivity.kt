aeb7346e64418f3dc174f8f7deef76dcc085de43
David <d1megaenterprise@gmail.com>
2020-12-08 11:53:46 +0100

    Added notes data to NoteListActivity Listview Ui

--- app/src/main/java/com/example/notekeeper/NoteListActivity.kt
+++ app/src/main/java/com/example/notekeeper/NoteListActivity.kt
@@ -2,10 +2,23 @@ package com.example.notekeeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class NoteListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_list)
+
+        initializeDisplayContent()
+
+    }
+
+    private fun initializeDisplayContent() {
+        val listnotes: ListView = findViewById(R.id.list_notes)
+        var notes: List<NoteInfo> = DataManager.getInstance().notes
+        val adapterNotes: ArrayAdapter<NoteInfo>? =
+            notes?.let { ArrayAdapter(this, android.R.layout.simple_list_item_1, it) }
+        listnotes.adapter = adapterNotes
     }
 }
